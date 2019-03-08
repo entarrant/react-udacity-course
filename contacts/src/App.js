5636b83fca342141043490e0e935ef71df537241
Emily Tarrant <emily.tarrant@bigcommerce.com>
2019-03-08 08:36:04 -0600

    Add naive ContactList component

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.people;
+
+    return (
+      <ol>
+        {people.map(person => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ol>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ContactList
+          people={[{ name: "Ben" }, { name: "Stevie " }, { name: "Shaun" }]}
+        />
+        <ContactList
+          people={[{ name: "Emily" }, { name: "Kevin" }, { name: "Aaron" }]}
+        />
+        <ContactList people={[{ name: "Andrew" }]} />
       </div>
     );
   }
