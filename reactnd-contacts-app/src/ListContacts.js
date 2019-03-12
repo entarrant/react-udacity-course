96d37e0ac06391898658e63adc632c299017b373
Emily Tarrant <entarrant@gmail.com>
2019-03-12 09:29:14 -0500

    Add search bar that is bound to ListContacts state

--- reactnd-contacts-app/src/ListContacts.js
+++ reactnd-contacts-app/src/ListContacts.js
@@ -1,34 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-function ListContacts(props) {
-  return (
-    <ol className="contact-list">
-      {props.contacts.map(contact => (
-        <li key={contact.id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{ backgroundImage: `url(${contact.avatarURL})` }}
-          />
-          <div className="contact-details">
-            <p>{contact.name}</p>
-            <p>{contact.handle}</p>
-          </div>
-          <div
-            className="contact-remove"
-            onClick={() => props.removeContact(contact)}
-          >
-            Remove
-          </div>
-        </li>
-      ))}
-    </ol>
-  );
-}
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    removeContact: PropTypes.func.isRequired
+  };
 
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  removeContact: PropTypes.func.isRequired
-};
+  state = {
+    query: ""
+  };
+
+  updateQuery = query => {
+    this.setState({ query: query.trim() });
+  };
+
+  render() {
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search contacts"
+            value={this.state.query}
+            onChange={event => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className="contact-list">
+          {this.props.contacts.map(contact => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{ backgroundImage: `url(${contact.avatarURL})` }}
+              />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <div
+                className="contact-remove"
+                onClick={() => this.props.removeContact(contact)}
+              >
+                Remove
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
 
 export default ListContacts;
