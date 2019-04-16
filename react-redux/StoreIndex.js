61931d35f5f5da2614713a0e5788c95493ef0bfa
Emily Tarrant <entarrant@gmail.com>
2019-04-16 14:51:57 -0500

    Add dispatch method

--- react-redux/StoreIndex.js
+++ react-redux/StoreIndex.js
@@ -1,4 +1,15 @@
-function createStore() {
+// App code
+function todos(state = [], action) {
+  // A reducer function!
+  if (action.type === "ADD_TODO") {
+    return state.concat([action.todo]);
+  }
+
+  return state;
+}
+
+// Library Code
+function createStore(reducer) {
   /* The store should have four parts:
      Manage the state
      Get the state
@@ -6,6 +17,10 @@ function createStore() {
      Update the state
 
      Only an event can change the state of the store
+     The function (a reducer) that returns the new state needs to be a pure function
+      - Returns the same result if the same arguments are passed in
+      - Depends solely on the arguments passed in to them (doesn't use variables outside its scope)
+      - Does not produce any side effects
   */
 
   let state;
@@ -22,8 +37,15 @@ function createStore() {
     };
   };
 
+  const dispatch = action => {
+    state = reducer(state, action);
+
+    listeners.forEach(listener => listener());
+  };
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch
   };
 }
