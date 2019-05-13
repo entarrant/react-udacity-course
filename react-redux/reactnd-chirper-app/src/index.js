ae9a8f98d183c53b1a693b51f0a5f4f4dc9fafef
Emily Tarrant <entarrant@gmail.com>
2019-05-13 13:57:27 -0500

    Add thunk and logger middleware

--- react-redux/reactnd-chirper-app/src/index.js
+++ react-redux/reactnd-chirper-app/src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./components/App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducer from "./reducers";
 
-const store = createStore(reducer);
+import reducer from "./reducers";
+import middleware from "./middleware";
+
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
