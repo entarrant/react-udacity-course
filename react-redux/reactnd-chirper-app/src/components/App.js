a8eef2cfd6454b9c6d9c2aad8ba9291857d8fdd8
Emily Tarrant <entarrant@gmail.com>
2019-05-13 15:03:50 -0500

    Render rudimentary Dashboard component state once data is loaded

--- react-redux/reactnd-chirper-app/src/components/App.js
+++ react-redux/reactnd-chirper-app/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import Dashboard from "../components/Dashboard";
 
 class App extends Component {
   componentDidMount() {
@@ -8,8 +9,14 @@ class App extends Component {
   }
 
   render() {
-    return <div>Starter Code</div>;
+    return <div>{this.props.loading ? null : <Dashboard />}</div>;
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authUser }) {
+  return {
+    loading: authUser === null
+  };
+}
+
+export default connect(mapStateToProps)(App);
