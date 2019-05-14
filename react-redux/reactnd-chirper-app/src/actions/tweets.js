2b4880bec9c86e6fb08e62360e5ee42b15337c55
Emily Tarrant <entarrant@gmail.com>
2019-05-14 08:28:17 -0500

    Add action creators for toggling tweet likes

--- react-redux/reactnd-chirper-app/src/actions/tweets.js
+++ react-redux/reactnd-chirper-app/src/actions/tweets.js
@@ -1,8 +1,32 @@
+import { saveLikeToggle } from "../utils/api";
+
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
+export const TOGGLE_TWEET = "TOGGLE_TWEET";
 
 export function receiveTweets(tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets
+  };
+}
+
+export function toggleTweet({ id, authUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authUser,
+    hasLiked
+  };
+}
+
+export function handleToggleTweet(tweetInfo) {
+  return dispatch => {
+    dispatch(toggleTweet(tweetInfo));
+
+    return saveLikeToggle(tweetInfo).catch(e => {
+      console.warn(`Could not toggle tweet ${tweetInfo.id}: ${e}`);
+      dispatch(toggleTweet(tweetInfo));
+      alert("There was an error toggling the tweet. Please try again.");
+    });
   };
 }
